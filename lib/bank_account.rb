ef801847874fd1fba141eb06568e167c43d992aa
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-16 16:53:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,27 +1,23 @@
-class BankAccount
-  attr_accessor :balance, :status 
+class BankAccount 
+  
+  attr_accessor :balance, :status
   attr_reader :name 
-
-def initialize(account_name)
-  @name = account_name 
-  @balance = 1000
-  @status = "open"
-end 
-
-def deposit(cash)
-  @balance += (cash)
-end 
-
-def display_balance
-  "Your balance is $#{@balance}." 
-end
-
-def valid?
-  @status == "open" && @balance > 0 ? true:false 
-end
-
-def close_account
-  @status = "closed"
-end 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open"
+  end
+  
+  def deposit(cash)
+    @balance += (cash)
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
 
 end 
+
+name = BankAccount.new("Billy")
+puts name 

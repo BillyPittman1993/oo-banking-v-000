1f86230f671a01f9952a12b7a65d670c8886369a
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-16 17:15:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,11 @@
 class Transfer 
   
   
-  def initialize(transfer)
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
   end 
   
 end 
